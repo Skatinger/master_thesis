f017d8d0d48ccb8f0e079c1ef20b136f073b759f
Nya <alex.nyffenegger@app.ch>
2022-05-18 14:58:36 +0200

    add ubelix job script for paraphrasing

---
+++ wiki_poc/dataset/build-paraphrased.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name="wiki-top-50-predicitions"
+#SBATCH --time=00:59:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --partition=gpu
+#SBATCH --qos=job_gpu_preempt
+#SBATCH --gres=gpu:rtx3090
+#SBATCH --mail-user=alex.nyffenegger@outlook.com
+
+# Your code below this line
+module load Anaconda3
+module load Workspace
+module load CUDA
+eval "$(conda shell.bash hook)"
+conda activate standard-nlp
+
+srun python3 build-paraphrased.py
