2e8eb77f8bb17828ba8f036349069ea7d1f4ab29
Nya <alex.nyffenegger@app.ch>
2022-05-19 21:46:01 +0200

    add NER and masking

---
+++ wiki_poc/dataset/build-masked.py
@@ -0,0 +1,80 @@
+## recognizes entities in the given sentences and masks them accordingly
+## stores masked sentences and the tokens belonging to the masks
+
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+import logging
+from ast import literal_eval
+import os
+import functools
+import pandas as pd
+import re
+
+dataset_file = 'wiki-dataset.csv'
+
+@functools.lru_cache(maxsize=1)
+def load_ner_pipeline(model_name ="dslim/bert-base-NER"):
+    print(f"Loading {model_name}")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+    return pipeline("ner", model=model, tokenizer=tokenizer)
+
+def masking(ner_results, text, entity_to_mask, mask_token = '<mask>'):
+    person_nr = -1
+    entities = []
+    for entity in ner_results:
+        tag = entity['entity']
+        if 'PER' in tag:  # if it is a person
+            if 'B' in tag and '#' not in entity['word']:
+                person_nr += 1  # we came to the next person
+                entities.append(entity['word'].strip())
+            else:
+                if '#' in entity['word']:
+                    entities[person_nr] += entity['word'].strip().strip('#')
+                else:
+                    entities[person_nr] += ' ' + entity['word']
+
+    # remove entities which are not the ones we want to mask
+    for entity in entities:
+        if entity not in entity_to_mask or entity_to_mask not in entity:
+            entities.remove(entity)
+
+    # return a dataset of anonymized strings with the belonging entities
+    # the re.sub ## is required to concat BERT word splits back together
+    # https://stackoverflow.com/questions/69921629/transformers-autotokenizer-tokenize-introducing-extra-characters
+    return [re.sub(entity, mask_token, text) for entity in entities], entities # [re.sub(" ##", '', entity) for entity in entities]
+
+
+if __name__ == '__main__':
+    if not os.path.exists(dataset_file):
+        logging.error("Input file does not exist. Please run `download-wiki.py` first.")
+        quit()
+
+    # read in the wiki-dataset
+    dataset = pd.read_csv(dataset_file)
+    if not 'sentences' in dataset.columns:
+        logging.error("Input file does not contain a 'sentences' column. Please run `build-unparaphrased.py` first.")
+        quit()
+    
+    # parse sentences string back to list of sentences
+    dataset['sentences'] = dataset['sentences'].apply(literal_eval)
+    dataset['paraphrased_sentences'] = dataset['paraphrased_sentences'].apply(literal_eval)
+
+    # convert sentences to text
+    dataset['normal_text'] = dataset['sentences'].apply(lambda x: " ".join(x))
+    dataset['paraphrased_text'] = dataset['paraphrased_sentences'].apply(lambda x: " ".join(x))
+
+    # perform NER on the texts
+    ner = load_ner_pipeline()
+    normal_ner_results = dataset['normal_text'].apply(lambda x: ner(x))
+    paraphrased_ner_results = dataset['paraphrased_text'] = dataset['paraphrased_sentences'].apply(lambda x: ner(x))
+
+    # masking
+    for index, row in dataset.iterrows():
+        dataset.at[index, 'normal_masked_text'], dataset.at[index, 'normal_entities'] = masking(normal_ner_results[index], row['normal_text'], row['title'])
+        dataset.at[index, 'paraphrased_masked_text'], dataset.at[index, 'paraphrased_entities'] = masking(paraphrased_ner_results[index], row['paraphrased_text'], row['title'])
+
+    # save the dataset
+    dataset.to_csv('wiki-dataset-masked.csv', index=False)
+
