a63156f1d451a2dbc0363bc71792b8602e09b5b8
Alex Nyffenegger <alex.nyffenegger@unifr.ch>
2022-08-03 11:03:55 +0200

    update duration

--- wiki_poc/dataset/build-paraphrased.sh
+++ wiki_poc/dataset/build-paraphrased.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name="paraphrase-wiki-dataset"
-#SBATCH --time=00:20:00
+#SBATCH --time=08:00:00
 #SBATCH --mem-per-cpu=10G
 #SBATCH --partition=gpu
 #SBATCH --qos=job_gpu_preempt
