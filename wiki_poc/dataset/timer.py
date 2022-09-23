9e19c2b2081cab8729cb357b6351f618f66206b8
Nya <alex.nyffenegger@app.ch>
2022-09-23 11:39:10 +0200

    correct number of loops

--- wiki_poc/dataset/timer.py
+++ wiki_poc/dataset/timer.py
@@ -11,9 +11,9 @@ inputs = ["Hello, I am a text with a <mask>", "Wow, there is <mask> more text.",
 fill_mask = pipeline("fill-mask", model="roberta-base", tokenizer='roberta-base', top_k=5)
 
 
-d1 = timeit.timeit(lambda: [fill_mask(input) for input in inputs], number=1)
+d1 = timeit.timeit(lambda: [fill_mask(input) for input in inputs], number=1000)
 
-d2 = timeit.timeit(lambda: fill_mask(inputs), number=1)
+d2 = timeit.timeit(lambda: fill_mask(inputs), number=1000)
 
 print("Separated inputs took {}".format(d1))
 print("Joined inputs took    {}".format(d2))
