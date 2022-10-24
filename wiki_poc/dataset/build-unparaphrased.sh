c187ab9312156b66e952cc48d335d1c0dcdd8980
Alex Nyffenegger <alex.nyffenegger@unifr.ch>
2022-10-24 14:49:19 +0200

    change partition for non gpu-task

--- wiki_poc/dataset/build-unparaphrased.sh
+++ wiki_poc/dataset/build-unparaphrased.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name="unparaphrase-wiki-dataset"
-#SBATCH --time=00:10:00
+#SBATCH --job-name="build-unparaphrased-large-wiki-dataset"
+#SBATCH --time=00:03:00
 #SBATCH --mem-per-cpu=10G
 #SBATCH --partition=gpu
-#SBATCH --qos=job_gpu_preempt
-#SBATCH --gres=gpu:rtx3090
+#SBATCH --qos=job_epyc2
 #SBATCH --mail-user=alex.nyffenegger@outlook.com
 
 # Your code below this line
 module load Anaconda3
 module load Workspace
-module load CUDA
 eval "$(conda shell.bash hook)"
 conda activate standard-nlp
 
-srun python3 build-unparaphrased.py
+srun python3 build-unparaphrased-large.py
