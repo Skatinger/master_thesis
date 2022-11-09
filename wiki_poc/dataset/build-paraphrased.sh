97549dfd5b54c58ef6c2e6ba6ea2ac5524273c07
Nya <alex.nyffenegger@app.ch>
2022-11-09 17:39:00 +0100

    request more RAM

--- wiki_poc/dataset/build-paraphrased.sh
+++ wiki_poc/dataset/build-paraphrased.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="paraphrase-wiki-dataset"
 #SBATCH --time=10:00:00
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=50G
 #SBATCH --partition=gpu
 #SBATCH --qos=job_gpu_preempt
 #SBATCH --gres=gpu:rtx3090
