1e5937ff2e8c791dfdeb529fda950470852a1a00
Nya <alex.nyffenegger@app.ch>
2022-11-21 14:20:39 +0100

    add helper to concate datasets

---
+++ wiki_poc/dataset/concat-shards.py
@@ -0,0 +1,8 @@
+# small helper to reunite the shards after the paraphrasing
+from datasets import load_from_disk, concatenate_datasets
+
+shards = [load_from_disk("./data_paraphrased_shard_{}/".format(i)) for i in range(1, 8)]
+
+dataset = concatenate_datasets(shards)
+
+dataset.save_to_disk("./data_paraphrased")
