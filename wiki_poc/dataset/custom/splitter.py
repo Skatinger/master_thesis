60279bad4cd7ea77dc40e909d0f6df1cfeddbe12
Nya <alex.nyffenegger@app.ch>
2023-01-19 13:31:54 +0100

    update splitter

--- wiki_poc/dataset/custom/splitter.py
+++ wiki_poc/dataset/custom/splitter.py
@@ -24,24 +24,36 @@ class Splitter:
             yield chunkBatch
 
     # returns chunks of text evolving around a mask tokens, with text before and after the mask token
-    # specified by beforeLength and afterLength
+    # specified by beforeLength and afterLength. Any other mask tokens in the text are replaced with 'mask'
     # text: text to split
     # beforeLength: number of characters before the delimiter token
     # afterLength: number of characters after the delimiter token
     # delimiter: delimiter token, defaults to <mask>
     @classmethod
-    def split_around_mask(self, text, beforeLength, afterLength, delimiter=r'/<mask>/'):
+    def split_around_mask(self, text, beforeLength, afterLength, maskToken=r'<mask>'):
         # find all masks using regex
-        maskIndices = [m.start() for m in re.finditer(delimiter, text)]
+        maskIndices = [m.start() for m in re.finditer(maskToken, text)]
+        maskLength = len(maskToken)
         for maskIndex in maskIndices:
             # get the lengths of the text before and after the mask constrained by text boundaries
-            beforeIndex = max(0, maskIndex-beforeLength)
-            afterIndex = min(len(text) - 1, maskIndex + afterLength)
-            yield text[beforeIndex:afterIndex]
+            startIndex = max(0, maskIndex-beforeLength)
+            endIndex = min(len(text) - 1, maskIndex + maskLength + afterLength)
+            # move beforeIndex to start of word
+            while (startIndex > 0 and text[startIndex] != ' '):
+                startIndex -= 1
+            # move afterIndex to end of word
+            while (endIndex < len(text) - 1 and text[endIndex] != ' '):
+                endIndex += 1
+            # text before mask token with any other mask tokens removed
+            before = re.sub(maskToken, 'mask', text[startIndex:maskIndex])
+            # text after mask token with any other mask tokens removed
+            after = re.sub(maskToken, 'mask', text[maskIndex + maskLength:endIndex])
+            # yield the chunk of text
+            yield before + maskToken + after
 
     # TODO: implement
     # takes sentence endings into account, preventing sentences from being split, which might make
     # the prediction of some masks more difficult.
     @classmethod
     def split_by_sentences(self, text):
-        pass
+        raise NotImplementedError
