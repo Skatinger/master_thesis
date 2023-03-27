509cf608078d1a755260fb3cdf6f8fc2da7c4852
Nya <alex.nyffenegger@app.ch>
2023-03-27 11:15:53 +0200

    add processing function for gpt3.5 prompts

---
+++ wiki_poc/models/gpt_3_5_turbo.py
@@ -0,0 +1,78 @@
+import os
+import openai
+from datasets import load_dataset
+from datasets import Dataset
+import logging
+from tqdm.auto import tqdm
+openai.organization = "org-JkswNfkhKMfjPPgyLUjElGPH"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+"""
+This script is used to test the GPT-3.5-turbo model on the wikipedia dataset.
+Mask-Filling is not possible, so we use the ChatCompletion API to prompt for the name referred to as <mask>.
+
+Unlike other models, gpt-3.5-turbo usually detects the person best using the first few paragraphs of the article.
+Therefore we only pass in the first sequence of the article to the model. If it does not recognize the person,
+we pass in the first 500 characters of the second sequence as well.
+
+TODO: check where the cutoff is for the model to recognize the person, e.g. how many characters are needed and
+at what point is it useless to send any more characters to the model. This improves cost efficiency.
+"""
+
+
+if __name__ == "__main__":
+
+    # ensure required test ids file exists
+    assert os.path.exists("test_set_ids.csv"), "test_set_ids.csv file not found. Please run generate_test_set_ids.py first."
+    # ensure api key is set
+    assert openai.api_key is not None, "OPENAI_API_KEY environment variable not set."
+
+    CONFIG = "original"
+    MODEL_NAME = "gpt-3.5-turbo"
+
+    user_prompt = "Who is the person refered to as <mask>? Only give the exact name without punctuation."
+
+    # dataset with initial pages
+    dataset = load_dataset('Skatinger/wikipedia-persons-masked', CONFIG, split='train')
+    # get set of page ids which are in the test_set_ids.csv file
+    test_set_ids = set([i.strip() for i in open("test_set_ids.csv").readlines()])
+    # filter out pages from dataset which are not in the test set
+    dataset = dataset.filter(lambda x: x["id"] in test_set_ids)
+
+    # prepare result dataset
+    result_dataset = Dataset.from_dict({'prediction': [], 'page_id': [], 'input_length': []})
+
+    # temporary miniature shard for testing
+    dataset = dataset.shard(5000, 1)
+
+    # iterate over pages in dataset
+    for page in tqdm(dataset):
+        # extract text from page
+        text = page[f"masked_text_{CONFIG}"][:1000]
+        # prompt openai api for prediction
+        response = openai.ChatCompletion.create(
+            model=MODEL_NAME,
+            messages=[
+                { "role": "user", "content": text + " " + user_prompt },
+            ],
+            temperature=0.5,
+            max_tokens=10,
+            top_p=1,
+            n=1,
+            frequency_penalty=0, 
+            presence_penalty=0,
+            stop=["\n"]
+        )
+        # add prediction to result dataset
+        result_dataset = result_dataset.add_item(
+            {'prediction': response['choices'][0]['message']['content'],
+             'page_id': page['id'], 'input_length': len(text)})
+        # print response for debugging
+        print(response)
+
+    # save dataset
+    import pdb; pdb.set_trace()
+    PATH = f"wiki_predictions_{MODEL_NAME.replace('/', '_')}_{CONFIG}.jsonl"
+    logging.info('Saving dataset to path %s', PATH)
+    result_dataset.to_json(PATH)
