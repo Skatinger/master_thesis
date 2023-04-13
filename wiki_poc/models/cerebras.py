cc4f4a899a34213ac2a31e5620f3ab9756c05663
Nya <alex.nyffenegger@app.ch>
2023-04-13 14:13:14 +0200

    remove unused code

--- wiki_poc/models/cerebras.py
+++ wiki_poc/models/cerebras.py
@@ -80,13 +80,3 @@ if __name__ == "__main__":
     # save final dataset to file
     logging.info('Saving final dataset to path %s', PATH)
     result_dataset.save_to_disk(PATH)
-
-
-    # text = "Generative AI is "
-
-    # inputs = tokenizer(text, return_tensors="pt")
-    # outputs = model.generate(**inputs, num_beams=5, 
-    #                         max_new_tokens=10, early_stopping=True,
-    #                         no_repeat_ngram_size=2)
-    # text_output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    # print(text_output[0])
