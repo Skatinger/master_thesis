7dd922d20ec8f664f16284e6b06bf57471652f0b
Nya <alex.nyffenegger@app.ch>
2023-05-22 12:56:55 +0200

    naming convention, batch sizes

--- wiki_poc/models/runners/t5/t5_runner.py
+++ wiki_poc/models/runners/t5/t5_runner.py
@@ -11,9 +11,9 @@ class T5Runner(AbstractTextToTextRunner):
     @staticmethod
     def names():
         return {
-            "t5-small": "t5-small",
-            "t5-base": "t5-base",
-            "t5-large": "t5-large",
+            "t5-0b060": "t5-small",
+            "t5-0b220": "t5-base",
+            "t5-0b770": "t5-large",
             "t5-3b": "t5-3b",
             "t5-11b": "t5-11b",
         }
@@ -21,9 +21,9 @@ class T5Runner(AbstractTextToTextRunner):
     @staticmethod
     def sizes():
         return {
-            "XXS": "t5-small",
-            "XS": "t5-base",
-            "M": "t5-large",
+            "XXS": "t5-0b060",
+            "XS": "t5-0b220",
+            "M": "t5-0b770",
             "L": "t5-3b",
             "XL": "t5-11b",
         }
@@ -31,8 +31,8 @@ class T5Runner(AbstractTextToTextRunner):
     @staticmethod
     def batch_sizes():
         return {
-            "t5-small": 64,
-            "t5-base": 64,
+            "t5-small": 256,
+            "t5-base": 128,
             "t5-large": 64,
             "t5-3b": 64,
             "t5-11b": 64,
