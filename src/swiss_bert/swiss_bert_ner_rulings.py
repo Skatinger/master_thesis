de38fa33a1b3b9176fe5f7df3704756ce45b02a5
Nya <alex.nyffenegger@app.ch>
2023-05-31 23:25:48 +0200

    initial setup

---
+++ src/swiss_bert/swiss_bert_ner_rulings.py
@@ -0,0 +1,12 @@
+# script to recognize entities in swiss court rulings
+
+# TODO:
+# - find a subset of decisions
+# - mask the concerning persons
+# - make mask filling with swissBert
+# 
+# 
+
+
+# start with very small subset of manually reidentified
+# 1. dj bobo articles + dj bobo decision
