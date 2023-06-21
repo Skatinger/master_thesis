89bcfad531c899175a856b7c71a214d2d4636de1
Nya <alex.nyffenegger@app.ch>
2023-06-21 13:46:31 +0200

    fix import

--- wiki_poc/models/runners/distillbert/distillbert_runner.py
+++ wiki_poc/models/runners/distillbert/distillbert_runner.py
@@ -1,6 +1,6 @@
 
 
-from .abstract_fill_mask_runner import AbstractFillMaskRunner
+from ..abstract_fill_mask_runner import AbstractFillMaskRunner
 from transformers import DistilBertModel, DistilBertTokenizer
 
 
