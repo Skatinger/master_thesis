6cd77aa67fbca1962f3d9f16173dd6b562a7f1ef
Nya <alex.nyffenegger@app.ch>
2023-06-28 09:30:11 +0200

    add more llama models

--- wiki_poc/models/runners/llama/huggy_llama_runner.py
+++ wiki_poc/models/runners/llama/huggy_llama_runner.py
@@ -6,18 +6,25 @@ class HuggyLlamaRunner(AbstractRunner):
     def names():
         return {
             "llama-7b": "huggyllama/llama-7b",
+            "llama-13b": "huggyllama/llama-13b",
+            "llama-30b": "huggyllama/llama-30b",
+            "llama-65b": "huggyllama/llama-65b",
         }
     
     @staticmethod
     def sizes():
         return {
             "L": "llama-7b",
-            # "XL": "llama-13b",
+            "XL": "llama-13b",
+            "XXL": "llama-30b",
+            "XXXL": "llama-65b",
         }
         
     @staticmethod
     def batch_sizes():
         return {
-            "llama-7b": 64,
-            # "llama-13b": 64,
+            "llama-7b": 8,
+            "llama-13b": 8,
+            "llama-30b": 4,
+            "llama-65b": 4,
         }
