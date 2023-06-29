d267e9ae429a0e2d895fd8444346feee8efad0f1
Nya <alex.nyffenegger@app.ch>
2023-06-29 09:10:46 +0200

    small fix

--- wiki_poc/models/runners/deberta/deberta_runner.py
+++ wiki_poc/models/runners/deberta/deberta_runner.py
@@ -1,15 +1,19 @@
 
 
 from ..abstract_fill_mask_runner import AbstractFillMaskRunner
-from transformers import DebertaForMaskedLM
+from transformers import DebertaV2ForMaskedLM, DebertaV2Tokenizer
+import torch
+import logging
 
 class DebertaRunner(AbstractFillMaskRunner):
 
     @staticmethod
     def names():
         return {
-            "mdeberta-0b086": "microsoft/mdeberta-v3-base",
+            # "mdeberta-0b086": "microsoft/mdeberta-v3-base",
             "deberta-0b304": "microsoft/deberta-v3-large",
+            # "deberta-sadf": "microsoft/deberta-large",
+            # "deberta": "microsoft/deberta-v2-xlarge"
         }
     
     @staticmethod
@@ -27,4 +31,13 @@ class DebertaRunner(AbstractFillMaskRunner):
         }
 
     def _model_loader(self):
-        return DebertaForMaskedLM
+        return DebertaV2ForMaskedLM
+
+    def get_model(self):
+        """override as device_map='auto' is not supported by distilbert"""
+        if torch.cuda.is_available():
+            logging.info(f"Loading model for {self.model_name}")
+            model_path = self.names()[self.model_name]
+            return self._model_loader().from_pretrained(model_path, torch_dtype=torch.float16).to(self.device)
+        else:
+            super().get_model()
