ae87ebe3d2128e4637b81f4203e52324d2052b82
Nya <alex.nyffenegger@app.ch>
2023-06-29 09:12:30 +0200

    fix device map problem

--- wiki_poc/models/runners/deberta/deberta_qa_runner.py
+++ wiki_poc/models/runners/deberta/deberta_qa_runner.py
@@ -1,7 +1,8 @@
 
 
 from ..abstract_qa_runner import AbstractQARunner
-
+import torch
+import logging
 class DebertaQARunner(AbstractQARunner):
 
     @staticmethod
@@ -21,3 +22,12 @@ class DebertaQARunner(AbstractQARunner):
         return {
             "deberta_squad-0b100": 64,
         }
+
+    def get_model(self):
+        """override as device_map='auto' is not supported by deberta"""
+        if torch.cuda.is_available():
+            logging.info(f"Loading model for {self.model_name}")
+            model_path = self.names()[self.model_name]
+            return self._model_loader().from_pretrained(model_path, torch_dtype=torch.float16).to(self.device)
+        else:
+            super().get_model()
