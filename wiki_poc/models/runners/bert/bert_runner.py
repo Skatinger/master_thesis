e73a390d8f6604fafda37f590f87e74e9be58d43
Nya <alex.nyffenegger@app.ch>
2023-07-07 10:08:07 +0200

    allow to set language header

--- wiki_poc/models/runners/bert/bert_runner.py
+++ wiki_poc/models/runners/bert/bert_runner.py
@@ -1,6 +1,8 @@
 
 from ..abstract_fill_mask_runner import AbstractFillMaskRunner
-from transformers import RobertaForMaskedLM, RobertaTokenizer
+from datasets import Dataset
+from pipelines.pt_utils import KeyDataset
+from tqdm import tqdm
 
 class BertRunner(AbstractFillMaskRunner):
 
@@ -22,4 +24,42 @@ class BertRunner(AbstractFillMaskRunner):
             "swiss_bert-0b110": 64,
             "xlm_swiss_bert-0b110": 64,
         }
+    
+    def run_pipe(self, dataset, pipe, config, batch_size=2):
+        """overwrite run_pipe to allow setting the correct language header"""
+        if not "language" in dataset.column_names:
+            pipe.model.set_default_language("en_XX")
+            return super().run_pipe(dataset, pipe, config, batch_size)
 
+        preds = {}
+        for i in range(self.k_runs):
+            preds[f"prediction_{i}"] = []
+        preds['input_id'] = []
+        preds['input_length'] = []
+        preds['language'] = []
+        result_dataset = Dataset.from_dict(preds)
+        languages = {
+            "de": "de_DE",
+            "fr": "fr_XX",
+            "it": "it_IT",
+        }
+        language_filtered_dataset = {}
+
+        # convert dataset to dataset shard for each language
+        for language, language_name in languages.items():
+            language_filtered_dataset[language] = dataset.filter(lambda x: x['language'] == language_name)
+
+        for language, filtered_dataset in language_filtered_dataset.items():
+            pipe.model.set_default_language(languages[language])
+            for example, out in zip(dataset, tqdm(pipe(KeyDataset(filtered_dataset, f"masked_text_{config}"), batch_size=batch_size))):
+                # get a prediction for every chunk in the batch
+                tokens, _scores = self.extract_result(out)
+                # split predictions to columns
+                item = self.convert_to_result(tokens)
+                # # add the predictions to the dataset
+                item['page_id'] = example['id']
+                item['language'] = example['language']
+                item['input_length'] = self.input_length
+                result_dataset = result_dataset.add_item(item)
+
+        return result_dataset
