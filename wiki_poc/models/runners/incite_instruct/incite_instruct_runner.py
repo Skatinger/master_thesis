35b6728c6541128bf3cb7d149ed3ab40cfe68e7c
Nya <alex.nyffenegger@app.ch>
2023-07-10 08:36:38 +0200

    add incite base model

--- wiki_poc/models/runners/incite_instruct/incite_instruct_runner.py
+++ wiki_poc/models/runners/incite_instruct/incite_instruct_runner.py
@@ -4,22 +4,26 @@ from ..abstract_runner import AbstractRunner
 class InciteInstructRunner(AbstractRunner):
     """
     https://huggingface.co/togethercomputer/RedPajama-INCITE-Instruct-3B-v1
+    https://huggingface.co/togethercomputer/RedPajama-INCITE-Base-3B-v1
     """
 
     @staticmethod
     def names():
         return {
-            "incite_instruct-3b": "togethercomputer/RedPajama-INCITE-Instruct-3B-v1"
+            "incite_instruct-3b": "togethercomputer/RedPajama-INCITE-Instruct-3B-v1",
+            "incite-3b": "togethercomputer/RedPajama-INCITE-Base-3B-v1",
         }
     
     @staticmethod
     def sizes():
         return {
             "M": "incite_instruct-3b",
+            "L": "incite-3b",
         }
 
     @staticmethod
     def batch_sizes():
         return {
             "incite_instruct-3b": 8,
+            "incite-3b": 8,
         }
