0f724cb02740a9d8554a5a9d000cb61386ff5818
Nya <alex.nyffenegger@app.ch>
2023-07-10 08:33:04 +0200

    add mt0 runner

---
+++ wiki_poc/models/runners/mt0/mt0_runner.py
@@ -0,0 +1,35 @@
+
+from ..abstract_runner import AbstractRunner
+
+
+class MT0Runner(AbstractRunner):
+
+    @staticmethod
+    def names():
+        return {
+            "mt0-0b300": "bigscience/mt0-small",
+            "mt0-0b580": "bigscience/mt0-base",
+            "mt0-1b2": "bigscience/mt0-large",
+            "mt0-3b7": "bigscience/mt0-xl",
+            "mt0-13b": "bigscience/mt0-xxl",
+        }
+    
+    @staticmethod
+    def sizes():
+        return {
+            "XS": "mt0-0b300",
+            "S": "mt0-0b580",
+            "M": "mt0-1b2",
+            "L": "mt0-3b7",
+            "XL": "mt0-13b",
+        }
+        
+    @staticmethod
+    def batch_sizes():
+        return {
+            "mt0-0b300": 32,
+            "mt0-0b580": 16,
+            "mt0-1b2": 8,
+            "mt0-3b7": 4,
+            "mt0-13b": 2,
+        }
