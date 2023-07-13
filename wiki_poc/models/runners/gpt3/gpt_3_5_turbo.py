3827ca3f79513a8b8d45fa36610ae0662577b85c
Nya <alex.nyffenegger@app.ch>
2023-07-13 13:32:59 +0200

    update gpt35 to run rulings

--- wiki_poc/models/runners/gpt3/gpt_3_5_turbo.py
+++ wiki_poc/models/runners/gpt3/gpt_3_5_turbo.py
@@ -3,44 +3,46 @@ import openai
 from datasets import load_dataset
 from datasets import Dataset
 import logging
+import sys
 from tqdm.auto import tqdm
 import time
 openai.organization = "org-JkswNfkhKMfjPPgyLUjElGPH"
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
+from models.model_runner import load_test_set
 
 """
-This script is used to test the GPT-3.5-turbo model on the wikipedia dataset.
+This script is used to test the GPT-3.5-turbo model on the datasets.
 Mask-Filling is not possible, so we use the ChatCompletion API to prompt for the name referred to as <mask>.
-
-Unlike other models, gpt-3.5-turbo usually detects the person best using the first few paragraphs of the article.
-Therefore we only pass in the first sequence of the article to the model. If it does not recognize the person,
-we pass in the first 500 characters of the second sequence as well.
-
-TODO: check where the cutoff is for the model to recognize the person, e.g. how many characters are needed and
-at what point is it useless to send any more characters to the model. This improves cost efficiency.
 """
-
 
 if __name__ == "__main__":
 
+    dataset = sys.argv[1]
+
+    if dataset == "rulings":
+        CONFIG = "original"
+        ids_file_path = "../../../test_set_ids_rulings.csv"
+    elif dataset == "wikipedia":
+        CONFIG = "paraphrased"
+        ids_file_path = "../../../test_set_ids.csv"
+
+    else:
+        raise ValueError("Please provide either 'rulings' or 'wikipedia' as first argument.")
+
     # ensure required test ids file exists
-    assert os.path.exists("../../../test_set_ids.csv"), "test_set_ids.csv file not found. Please run generate_test_set_ids.py first."
+    assert os.path.exists(ids_file_path), f"{ids_file_path} file not found. Please run generate_test_set_ids.py for your dataset first."
     # ensure api key is set
     assert openai.api_key is not None, "OPENAI_API_KEY environment variable not set."
 
-    CONFIG = "paraphrased"
     MODEL_NAME = "gpt-3.5-turbo-0613"
-    PATH = f"wiki_predictions_{MODEL_NAME.replace('/', '_')}_{CONFIG}.jsonl"
+    PATH = f"{dataset}_predictions_{MODEL_NAME.replace('/', '_')}_{CONFIG}.jsonl"
 
-    user_prompt = "Who is the person refered to as <mask>? Only give the exact name without punctuation. You are not allowed to respond with anything but the name, no more than 3 words."
+    user_prompt = """Who is the person refered to as <mask>? Only give the exact name without punctuation.
+                     You are not allowed to respond with anything but the name, no more than 3 words. If you don't know,
+                     give your best guess."""
 
     # dataset with initial pages
-    dataset = load_dataset('Skatinger/wikipedia-persons-masked', CONFIG, split='train')
-    # get set of page ids which are in the test_set_ids.csv file
-    test_set_ids = set([i.strip() for i in open("../../../test_set_ids.csv").readlines()])
-    # filter out pages from dataset which are not in the test set
-    dataset = dataset.filter(lambda x: x["id"] in test_set_ids)
+    dataset = load_test_set(dataset_type=dataset)
 
     # only process pages which have not been processed yet
     if os.path.exists(PATH):
@@ -58,7 +60,7 @@ if __name__ == "__main__":
     for index, page in enumerate(tqdm(dataset)):
         
         # extract text from page
-        text = page[f"masked_text_{CONFIG}"][:1000]
+        text = page[f"masked_text_{CONFIG}"]
         # prompt openai api for prediction
         try:
             response = openai.ChatCompletion.create(
