c88a73c8178176ae883bf168f836a4359aab4920
Nya <alex.nyffenegger@app.ch>
2023-08-05 11:06:23 +0200

    fix test set selection process

--- wiki_poc/dataset/rulings/rulings.py
+++ wiki_poc/dataset/rulings/rulings.py
@@ -1,8 +1,7 @@
 """masking of the rulings dataset"""
 
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 import re
-
 
 # TODO: add language for xlm models to set default language
 # TODO: some type of paraphrasing to make text shorter
@@ -11,6 +10,23 @@ class RulingsPreparer():
 
     def __init__(self, dataset: Dataset) -> Dataset:
         self.mask_regex = r"A._"
+
+    @staticmethod
+    def select_test_set_ids():
+        # finds well suited court rulings for testing and saves their id to a file
+        dataset = load_dataset("rcds/swiss_rulings", split="train")
+
+        # filter for rulings from the swiss federal court
+        dataset = dataset.filter(lambda x: x["court"] == "CH_BGer", num_proc=8)
+
+        # filter for rulings from 2019
+        dataset = dataset.filter(lambda x: x["year"] == 2019, num_proc=8)
+
+        # extract ids and save to file
+        ids = dataset["decision_id"]
+        with open("test_set_ids_rulings.csv", "w") as f:
+            for id in ids:
+                f.write(f"{id}\n")
     
     def prepare_rulings(self, dataset):
         # use id as identifier for consistency
@@ -34,3 +50,9 @@ class RulingsPreparer():
     def mask_dataset(self, dataset):
         dataset = dataset.map(self.mask_example)
         return dataset
+
+def main():
+    RulingsPreparer.select_test_set_ids()
+
+if __name__ == "__main__":
+    main()
