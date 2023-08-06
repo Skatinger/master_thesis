15b36ac80ca25b41cf50edb19e0e4527c9fe27d0
Nya <alex.nyffenegger@app.ch>
2023-08-07 01:59:27 +0200

    update evaluation

--- wiki_poc/evaluation/process_legal_results.py
+++ wiki_poc/evaluation/process_legal_results.py
@@ -12,9 +12,10 @@ name_dataset = NameDataset()
 
 global classification
 classification = {
-        "exist_in_text": {},
-        "not_a_name": {},
-        "just_a_letter": {},
+        "exists in text": {},
+        "not a name": {},
+        "no prediction": {},
+        "just a letter": {},
         "good": {},
     }
 
@@ -28,23 +29,25 @@ def join_predictions(row, name):
     row = row.to_dict()
     string = ""
     for key, value in row.items():
-        if key.startswith("prediction") and value not in row["masked_text_original"]:
+        if value is None:
+            classification["no prediction"][name] += 1
+        elif key.startswith("prediction") and value not in row["masked_text_original"]:
             if is_name(value):
                 string += f" {value}"
                 classification["good"][name] += 1
             else:
                 # 2 letters or less without whitespace are not names
                 if len(value.replace(" ", "")) < 3:
-                    classification["just_a_letter"][name] += 1
+                    classification["just a letter"][name] += 1
                 else:
-                    classification["not_a_name"][name] += 1
+                    classification["not a name"][name] += 1
         else:
             # value is already in masked_text_original
-            classification["exist_in_text"][name] += 1
+            classification["exists in text"][name] += 1
     return string
 
 def main():
-    assert len(sys.argv) > 1, "Please provide a path to the result file as an argument"
+    assert len(sys.argv) > 1, "Please provide a name of results folder as an argument"
 
     dataset_key = sys.argv[1]
 
@@ -66,14 +69,24 @@ def main():
     # Rename the column "decision_id" to "page_id" in full_rulings_df to match with other dataframes
     full_rulings_df = full_rulings_df.rename(columns={"decision_id": "page_id"})
 
+    if full_rulings_df['page_id'].duplicated().any():
+        print("Duplicates found in 'page_id' of 'full_rulings_df'")
+
+
     model_columns = []
     for model_class_name, model_class in predictions.items():
         for model_size, model in model_class.items():
+            print(f"processing model {model_class_name}-{model_size}")
             # Convert the predictions dataset to a pandas dataframe
             predictions_df = model['original']['train'].to_pandas()
 
             # join the ground truth onto the predictions
             predictions_df = pd.merge(predictions_df, gt_df, left_on="page_id", right_on="id")
+
+            # swiss_bert accidentally predicted some rows twice due some special handling.
+            # remove the duplicate rows
+            if predictions_df['page_id'].duplicated().any():
+                predictions_df = predictions_df.drop_duplicates(subset=['page_id'])
 
             # prepare classification dict
             for key in classification.keys():
@@ -103,7 +116,7 @@ def main():
     del final_df['total_length']
 
     # Save the final dataframe to a CSV file
-    final_df.to_csv(f"{key}-for-BG.csv", index=False)
+    final_df.to_csv(f"{dataset_key}-for-BG.csv", index=False)
 
     # plot a histogram of the lengths of classification categories
     labels = list(classification.keys())
@@ -114,28 +127,49 @@ def main():
     models.sort(key=lambda x: total_scores[x], reverse=True)
 
     x = np.arange(len(labels))  # the label locations
-    width = 0.15  # the width of the bars
+    width = 0.13  # the width of the bars
 
-    fig, ax = plt.subplots(figsize=(12, 8))
+    fig, ax = plt.subplots(figsize=(12, 9))
     rects = []
     for i, model in enumerate(models):
         model_values = [classification[label][model] for label in labels]
         rects.append(ax.bar(x - width*len(models)/2 + i*width, model_values, width, label=model))
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_title('Type of prediction by group and model', fontsize=22)
+    # ax.set_title('Type of prediction by model', fontsize=22)
     ax.set_xticks(x)
-    ax.set_xticklabels(labels, fontsize=16)
-    ax.legend(fontsize=12)
+    ax.set_xticklabels(labels, fontsize=20)
+    # sent font size of y axis ticks to 20
+    ax.tick_params(axis='y', labelsize=20)
+    legend = ax.legend(fontsize=20)
+    # legend.set_title('models', prop={'size': 26})
 
     # Move x-tick labels to the center of the sections
     for tick in ax.get_xticklabels():
         tick.set_ha('center')
 
-    plt.xlabel('Classified as', fontsize=18)
-    plt.ylabel('number of predictions', fontsize=18)
+    plt.xlabel('Classified as', fontsize=20)
+    plt.ylabel('number of predictions', fontsize=20)
 
-    plt.savefig(f"{dataset_key}-histogram.png")
+    plt.savefig(f"evaluation/plotting/plots/legal/{dataset_key}-histogram.png", bbox_inches='tight')
+
+    # pie plot for every model
+    # fix, axs = plt.subplots(nrows=2, ncols=3, figsize=(15,10))
+    # flat_axs = axs.flatten()
+
+    # import pdb; pdb.set_trace()
+
+    # for i, model in enumerate(models):
+    #     pieces = [classification[label][model] for label in labels]
+    #     # plot pieplot
+    #     flat_axs[i].pie(pieces, labels)
+    #     flat_axs[i].set_title(model)
+
+    # fig.legend()
+
+    # plt.savefig(f"evaluation/plotting/plots/legal/{dataset_key}-pie.png")
+
+
 
 if __name__ == "__main__":
     main()
