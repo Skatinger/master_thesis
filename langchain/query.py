3f56415b95a4872db0c3e8aa247dbe6b538a42d2
Nya <alex.nyffenegger@app.ch>
2023-08-08 01:40:25 +0200

    add additional code to query method for langchain

--- langchain/query.py
+++ langchain/query.py
@@ -2,12 +2,19 @@
 import  os
 from langchain.chains import VectorDBQA
 from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
 # import openai embeddings
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.qa import RetrievalQA
 
 persist_directory =  'db'
 embedding = OpenAIEmbeddings()
 
 # Now we can load the persisted database from disk, and use it as normal. 
 vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=vectordb)
+
+retriever = vectordb.as_retriever()
+
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever)
+
